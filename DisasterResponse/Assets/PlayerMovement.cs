74073b109f9c9f9a6969926d7bebe60f96e07481
Matthew Connolly <conn6273@csumb.ad.csumb.edu>
2018-11-03 00:12:05 -0700

    Added player movement based on mouse click and added A* asset

---
+++ DisasterResponse/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float speed = 10.0f;
+    private Vector2 target;
+    private Vector2 position;
+    private Camera cam;
+	// Use this for initialization
+	void Start () {
+        target = new Vector2(0.0f, 0.0f);
+        position = transform.position;
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    MovePlayer();
+        //    Debug.Log("Pressed");
+        //}
+	}
+
+    private void OnGUI()
+    {
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+        Vector2 point = new Vector2();
+
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0.0f));
+        if(Input.GetMouseButtonDown(0))
+        {
+            target = point;
+        }
+    }
+}
