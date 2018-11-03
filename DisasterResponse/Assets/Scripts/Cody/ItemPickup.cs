ab6749faa9035d5e7d0a149b5eb73f07a9efaa6c
Cody Regalado <rega5312@csumb.ad.csumb.edu>
2018-11-03 13:57:00 -0700

    adding some inventory UI scripts

---
+++ DisasterResponse/Assets/Scripts/Cody/ItemPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+
+    public Item item;
+
+    //Todo: create interactable script, which may call methods from this script
+
+    public void PickUp()
+    {
+
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            //remove from interactable object
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PickUp();
+        }
+	}
+}
