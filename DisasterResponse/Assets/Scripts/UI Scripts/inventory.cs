2c0c4f8756048f9a7b65af34033c01da58d96360
Cody Regalado <rega5312@csumb.ad.csumb.edu>
2018-11-03 02:14:26 -0700

    adding inventory UI

---
+++ DisasterResponse/Assets/Scripts/UI Scripts/inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour {
+
+    public item[] items = new item[9];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
