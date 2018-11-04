584b0981712210a763c9a8d8844f493076071034
Eric Tinoco <tino5168@csumb.ad.csumb.edu>
2018-11-04 05:19:27 -0800

    fire animation, and spawning of fire done

---
+++ DisasterResponse/Assets/Disasters/Fire/Script/Fire.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    private Animator anim;      // To hold the fire's animator
+    private float fire_timer;   // To hold the timer for the fire
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();    // set anin to components animator
+        anim.Play("small_fire");            // set start animation at small fire
+        fire_timer = 0;                     // set timer to 0
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FireLife();     // Determines life of fire
+	}
+
+    /// <summary>
+    /// FireLife method: Determines life of fire
+    /// </summary>
+    private void FireLife()
+    {
+        fire_timer++;       // increment fire timer
+
+        // if fire timer is less than zero, destroy object
+        if (fire_timer < 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // Fire get's stronger the longer it lives
+        if (fire_timer < 1800)
+        {
+            anim.Play("small_fire");
+        }
+        else if (fire_timer > 1800 && fire_timer < 3600)
+        {
+            anim.Play("medium_fire");
+        }
+        else if (fire_timer > 3600)
+        {
+            anim.Play("large_fire");
+        }
+        Debug.Log(fire_timer);
+
+        fire_timer = (fire_timer > 4200) ? 4200 : fire_timer;   // fire_timer can't pass 70 seconds
+    }
+    
+    /// <summary>
+    /// ReduceFire method: Reduce the fire life span by, reduction
+    /// </summary>
+    /// <param name="reduction"></param>
+    public void ReduceFire(float reduction)
+    {
+        fire_timer -= reduction;
+    }
+
+}
---
+++ DisasterResponse/Assets/Disasters/Fire/Script/FireInstantiation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireInstantiation : MonoBehaviour {
+    
+    public Transform[] fire_locations;
+    public GameObject firePrefab;
+
+    private float fire_timer;
+    private int fireIndex;
+    public List<int> current_locations = new List<int>();
+    private Vector2 fire_placement;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnFire();
+        fire_timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fire_timer++;
+
+        if(fire_timer % 3600 == 0 && current_locations.Count < fire_locations.Length)
+        {
+            SpawnFire();
+        }
+    }
+
+    /// <summary>
+    /// This function get's an index that isnt being used by other enemies.
+    /// It stores an integer into a List<int> current_locations.
+    /// It makes sure to return a value that isnt within the List.
+    /// </summary>
+    /// <returns>
+    /// An integer, from 0 - waypoints.length (0-14)
+    /// </returns>
+    private int GetIndex()
+    {
+        int index = Random.Range(0, fire_locations.Length);  // Declare and initialize index to be a random number from 0-14 (waypoint.length)
+
+        // while loop: Validation method that if the integer "index" is already in the List "current_locations" then,
+        // try for a new random integer, until one is not located in that list.
+        while (current_locations.Contains(index))
+        {
+            index = Random.Range(0, fire_locations.Length);
+        }
+
+        current_locations.Add(index);   // Add integer to List
+
+        return index;                   // Return integer
+    }
+
+    /// <summary>
+    /// This function will spawn all of the plushies at the start of the game
+    /// </summary>
+    void SpawnFire()
+    {                                                                  
+        fireIndex = GetIndex();
+        fire_placement = new Vector3(fire_locations[fireIndex].position.x, fire_locations[fireIndex].position.y, 0);
+        Instantiate(firePrefab, fire_placement, Quaternion.identity);
+    }
+}
